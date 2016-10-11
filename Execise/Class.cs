0ad04a2e21c679c0f65c910d707e02d43c640691
livenoz <huynhnguyenvietthanh1824@gmail.com>
2016-10-12 00:01:59 +0800

    Update code Improve

--- Execise/Class.cs
+++ Execise/Class.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Execise
 {
@@ -9,64 +10,70 @@ namespace Execise
         Japan = 3,
         China = 4
     }
+
     public class TaxCarModel
     {
+        public TaxCarModel()
+        {
+            Tax = DataDefault.NoValue;
+        }
         public Area Area { get; set; }
-        public double? FromCapacity { get; set; }
-        public double? ToCapacity { get; set; }
+        public double FromCapacity { get; set; }
+        public double ToCapacity { get; set; }
         public double Tax { get; set; }
     }
+
     public class CarModel
     {
         public Area Area { get; set; }
         public string Name { get; set; }
         public double Capacity { get; set; }
         public double PriceUsd { get; set; }
-
         public double PricePeso
+        {
+            get { return PriceUsd*DataDefault.PesoRate; }
+        }
+    }
+
+    public class CarBaseViewModel: CarModel
+    {
+        public CarBaseViewModel() { }
+        public CarBaseViewModel(CarModel car, double tax)
+        {
+            Area = car.Area;
+            Name = car.Name;
+            Capacity = car.Capacity;
+            PriceUsd = car.PriceUsd;
+            Tax = tax;
+        }
+        public double Tax { get; set; }
+        public double? Total
         {
             get
             {
-                return PriceUsd * DataDefault.PesoRate;
+                var retults = new[] {null, (double?)TotalValue };
+                return retults[CheckHaveTax];
             }
         }
-
-        
-    }
-
-    public abstract class CarBaseViewModel
-    {
-        public string Name { get; set; }
-        public double PricePeso { get; set; }
-        public double? Tax { get; set; }
-        public double? Total
+        public string MessageResult
         {
-            get { return PricePeso * (1 + Tax) * (1 + DataDefault.Vat); }
+            get
+            {
+                var results = new[] {"Invalid data", "No have tax", TotalValue.ToString("##,###.##")};
+                return results[CheckInvalidData + CheckHaveTax];
+            }
         }
-        public abstract string MessageResult { get;}
-    }
-
-    public class CarInvalidViewModel : CarBaseViewModel
-    {
-        public override string MessageResult
+        private int CheckInvalidData
         {
-            get { return "Invalid data"; }
+            get { return Convert.ToInt32(Capacity > 0 && PriceUsd >= 0); }
         }
-    }
-
-    public class CarNoTaxViewModel : CarBaseViewModel
-    {
-        public override string MessageResult
+        private int CheckHaveTax
         {
-            get { return "No have tax"; }
+            get { return Convert.ToInt32(Capacity > 0 && PriceUsd >= 0 && Tax >= 0); }
         }
-    }
-
-    public class CarValidViewModel : CarBaseViewModel
-    {
-        public override string MessageResult
+        private double TotalValue
         {
-            get { return Total.Value.ToString("##,###.##"); }
+            get { return PricePeso*(1 + Tax)*(1 + DataDefault.Vat); }
         }
     }
 
@@ -74,17 +81,18 @@ namespace Execise
     {
         public const int PesoRate = 47;
         public const double Vat = 0.12;
+        public const double NoValue = -1;
         public static List<TaxCarModel> TaxCars = new List<TaxCarModel>
         {
-            new TaxCarModel { Area = Area.Europe, Tax = 1, ToCapacity = 2 },
+            new TaxCarModel { Area = Area.Europe, Tax = 1, FromCapacity = double.MinValue, ToCapacity = 2 },
             new TaxCarModel { Area = Area.Europe, Tax = 1.2, FromCapacity = 2, ToCapacity = 5 },
-            new TaxCarModel { Area = Area.Europe, Tax = 2, FromCapacity = 5},
-            new TaxCarModel { Area = Area.Usa, Tax = 0.75, ToCapacity = 2 },
+            new TaxCarModel { Area = Area.Europe, Tax = 2, FromCapacity = 5, ToCapacity = double.MaxValue },
+            new TaxCarModel { Area = Area.Usa, Tax = 0.75, FromCapacity = double.MinValue, ToCapacity = 2 },
             new TaxCarModel { Area = Area.Usa, Tax = 0.9, FromCapacity = 2, ToCapacity = 5 },
-            new TaxCarModel { Area = Area.Usa, Tax = 1.5, FromCapacity = 5},
-            new TaxCarModel { Area = Area.Japan, Tax = 0.7, ToCapacity = 2 },
+            new TaxCarModel { Area = Area.Usa, Tax = 1.5, FromCapacity = 5, ToCapacity = double.MaxValue },
+            new TaxCarModel { Area = Area.Japan, Tax = 0.7, FromCapacity = double.MinValue, ToCapacity = 2 },
             new TaxCarModel { Area = Area.Japan, Tax = 0.8, FromCapacity = 2, ToCapacity = 5 },
-            new TaxCarModel { Area = Area.Japan, Tax = 1.35, FromCapacity = 5}
+            new TaxCarModel { Area = Area.Japan, Tax = 1.35, FromCapacity = 5, ToCapacity = double.MaxValue }
         };
         public static List<CarModel> Cars = new List<CarModel>
         {
--- Execise/Program.cs
+++ Execise/Program.cs
@@ -8,21 +8,14 @@ namespace Execise
     {
         public static void Main(string[] args)
         {
-            var listResult = new List<CarBaseViewModel>();
-            listResult.AddRange(DataDefault.Cars.Where(c => c.Capacity <= 0 || c.PriceUsd < 0)
-                .Select(c => new CarInvalidViewModel { Name = c.Name, PricePeso = c.PricePeso }));
-            var query = (from car in DataDefault.Cars.Where(c => c.Capacity > 0 && c.PriceUsd >= 0)
-                         join tax in DataDefault.TaxCars on car.Area equals tax.Area into lTax
-                         from tax in lTax.DefaultIfEmpty()
-                         where tax == null
-                               || ((!tax.FromCapacity.HasValue || car.Capacity > tax.FromCapacity)
-                                   && (!tax.ToCapacity.HasValue || car.Capacity <= tax.ToCapacity))
-                         select new { car.Name, car.PricePeso, TaxCar = tax }).ToList();
-            listResult.AddRange(query.Where(c => c.TaxCar == null)
-                .Select(c => new CarNoTaxViewModel { Name = c.Name, PricePeso = c.PricePeso }));
-            listResult.AddRange(query.Where(c => c.TaxCar != null)
-                .Select(c => new CarValidViewModel { Name = c.Name, PricePeso = c.PricePeso, Tax = c.TaxCar.Tax }));
-            foreach (var car in listResult)
+            var result = from car in DataDefault.Cars
+                join tax in DataDefault.TaxCars on car.Area equals tax.Area into lTax
+                from tax in lTax.DefaultIfEmpty(new TaxCarModel())
+                where tax.Tax.Equals(DataDefault.NoValue)
+                    || (car.Capacity > tax.FromCapacity && car.Capacity <= tax.ToCapacity)
+                select new CarBaseViewModel(car, tax.Tax);
+
+            foreach (var car in result)
             {
                 Console.WriteLine("{0}: {1}", car.Name, car.MessageResult);
             }
